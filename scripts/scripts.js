29c2736dbd90b487a311f0eb84b4fb410578dcf4
Carlyse Jordan <carlyse.jordan@Carlyses-MacBook-Air.local>
2017-10-08 18:10:29 -0400

    my introduction

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,7 @@
 module.exports = function(robocop){
-    robocop.hear(/welcome!/, function(res){
-        return res.send("Welcome and happy first birthday Zoe")
+    robocop.hear(/speak!  /, function(res){
+        return res.send(" Carlyse Jordan of the House Targaryen, First of Her Name, the Unburnt, Queen of the Andals and the First Men, Khaleesi of the Great Grass Sea, Breaker of Chains, and Mother of Dragons."
+            ")
+
     });
 }
